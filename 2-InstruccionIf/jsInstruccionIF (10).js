1f6ed5d8c43e04fe43729e9b3d75e4aac3d64fc9
DanFakePlasticTrees <34700952+DanFakePlasticTrees@users.noreply.github.com>
2019-02-14 12:15:40 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,9 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+		var numAleatorio = Math.round(Math.random()*10);
+	if (numAleatorio<4) {alert ("¡Vamos, la próxima se puede! \nLa nota es: " + numAleatorio );
+		}  else { if (numAleatorio >= 4 && numAleatorio <9 ){alert("¡Aprobó! \nLa nota es: " + numAleatorio);} 
+	else {alert("¡Excelente! \nLa nota es: " + numAleatorio);}}
 
 }//FIN DE LA FUNCIÓN
