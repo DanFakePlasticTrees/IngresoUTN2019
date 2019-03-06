a8172f71b611bea8d4c289b0ee1136477651e14a
DanFakePlasticTrees <34700952+DanFakePlasticTrees@users.noreply.github.com>
2019-03-05 22:36:53 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,5 +1,26 @@
 function Mostrar()
-{
+{ var numero;
+var divisores;
+var cantidadDiv = 0;
+
+numero = parseInt(prompt ("Ingrese un número: "))
+while (isNaN(numero) || numero < 0 ){
+    numero = parseInt(prompt ("Error. Ingrese un número: "))
+}
+for (var i = 0; i <= numero; i++) {
+
+if (numero%i == 0) {
+divisores = i;
+document.write(divisores + " es un divisor <br/>" )
+cantidadDiv++;
+
+}
+
+
+}
+
+document.write("La cantidad de divisores es: " + cantidadDiv)
+
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,17 @@
 function Mostrar()
-{
+{ var numero;
+    var numeroPrimo = 0;
 
+numero = parseInt(prompt ("Ingrese número: "));
+for (var i = 2 ; i < numero ; i++ ) {
+if (numero%i==0 ) {
+numeroPrimo++;
+}
+}
 
+if (numeroPrimo ==0) { document.write(numero + " es un número primo") } else{
+    document.write(numero + " no es un número primo")
+}
 
 
 }//FIN DE LA FUNCIÓN
